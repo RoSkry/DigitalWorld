8285320521645f4b75a5f7f80be14e41fea5af38
Rostyslav Skrypnyk <skrypnik-r@ukr.net>
2020-06-02 23:51:30 +0300

    Add simple DAL layer

---
+++ DigitalWorld.DAL/Model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalWorld.DAL.Model
+{
+   public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
